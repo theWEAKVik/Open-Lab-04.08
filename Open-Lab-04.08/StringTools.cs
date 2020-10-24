96f36db4fc9c944240ffa524cc72f807063d71aa
Viktor <krpectv57@gmail.com>
2020-10-24 13:41:09 +0200

    _

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +10,16 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            var stringy = new List<string>();
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                {
+                    stringy.Add(strings[i]);
+                }
+
+            }
+            return stringy.ToArray();
         }
     }
 }
